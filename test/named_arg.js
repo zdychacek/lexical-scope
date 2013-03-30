849ae8ce14383db43af64c9ae16262cb7b890970
James Halliday <mail@substack.net>
2013-03-30 00:52:43 -0700

    named arg test was actually fine

--- test/named_arg.js
+++ test/named_arg.js
@@ -1,16 +1,16 @@
 var test = require('tape');
 var detect = require('../');
 var fs = require('fs');
-var src = fs.readFileSync(__dirname + '/files/argument.js');
+var src = fs.readFileSync(__dirname + '/files/named_arg.js');
 
-test('parameters from inline arguments', function (t) {
+test('named argument parameter', function (t) {
     t.plan(3);
     
     var scope = detect(src);
     t.same(scope.globals.implicit, []);
     t.same(scope.globals.exported, []);
     t.same(scope.locals, {
-        'body.0': [ 'a' ],
+        'body.0': [ 'a', 'x' ],
         '': [ 'foo' ],
         'body.0.argument': [ 'c' ]
     });
