c33925c9db431e07dc5bff41943107b77ea51901
Ondrej Zdych <zdych.ondrej@gmail.com>
2017-07-17 09:52:43 +0200

    test: add object_spread test file

---
+++ test/object_spread.js
@@ -0,0 +1,13 @@
+var test = require('tape');
+var detect = require('../');
+var fs = require('fs');
+var src = fs.readFileSync(__dirname + '/files/object_spread.js');
+
+test('object spread', function (t) {
+    t.plan(3);
+
+    var scope = detect(src, { plugins: [ 'objectRestSpread' ] });
+    t.same(scope.globals.implicit, []);
+    t.same(scope.globals.exported, []);
+    t.same(scope.locals, { '': [ 'a' ] });
+});
